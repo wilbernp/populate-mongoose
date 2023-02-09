33b43060c98bdeb588523477b2c542628d2dffa3
wilbernp <wilbernarvaezpetro@gmail.com>
2023-02-09 12:05:33 -0500

    add populate for posts in getById

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -8,11 +8,23 @@ export default {
         return userCreated
     },
     getAll: async () => {
+        
         return await UserModel.find()
+        // con esto se elimina cualquier propiedad que se desee agregando el signo menos(-) antes del nombre
+        //  de la propiedad que se desea eliminar, para eliminar mas de una propiedad se separan con un
+        //  espacio ej: .select("-__v -_id")
+        .select("-__v")
     },
 
     getById: async (id:string) => {
         return await UserModel.findById(id)
+        .populate({
+            path:"posts",
+            // dentro del populate tambien se puede eliminar propiedades de esta forma
+            select:"-__v"
+        })
+        // esto se puede tambien aunque se use populate
+        .select("-__v")
     },
 
 }
