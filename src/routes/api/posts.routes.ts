e8e0676a73b72cdd6d26aa1104080f8c663abeb5
wilbernp <wilbernarvaezpetro@gmail.com>
2023-02-09 12:55:54 -0500

    add routes for create post

--- src/routes/api/posts.routes.ts
+++ src/routes/api/posts.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import commentController from "../../controllers/comment.controller";
 import postController from "../../controllers/post.controller";
 
 
@@ -8,6 +9,7 @@ const router = Router()
 router.post("/",postController.create)
 router.get("/",postController.getAll)
 router.get("/:id",postController.getById)
+router.post("/:postId/comments",commentController.create)
 
 
 export {router}
