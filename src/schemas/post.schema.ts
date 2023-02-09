3cec95175959752a70a18161c379033afd55891e
wilbernp <wilbernarvaezpetro@gmail.com>
2023-02-09 12:56:17 -0500

    add comments in Post schema

--- src/schemas/post.schema.ts
+++ src/schemas/post.schema.ts
@@ -4,6 +4,7 @@ import { IPost } from "../types/post";
 const PostSchema = new Schema<IPost>({
     content:String,
     author:{type:Schema.Types.ObjectId, ref:"User"},
+    comments:[{type:Schema.Types.ObjectId, ref:"Comment"}]
 },{timestamps:true})
 
 export const PostModel = model<IPost>("Post",PostSchema)
