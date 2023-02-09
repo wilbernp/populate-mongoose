2466eeed18547acc39bd138f0d7703068063b790
wilbernp <wilbernarvaezpetro@gmail.com>
2023-02-09 13:23:49 -0500

    add comments in create comment service

--- src/services/comments.service.ts
+++ src/services/comments.service.ts
@@ -6,8 +6,12 @@ export default {
     create: async (comment:IComment) => {
         const findPost = await postService.getById(comment.postId)
         const commentCreated = await new CommentModel(comment).save()
+
+        // se agrega el nuevo comentario al arreglo de comments del post correspondiente
+        // para que dicho array muestre todos los comentarios en posteriores consultas al modelo de posts
         findPost?.comments?.unshift(commentCreated._id)
         await findPost?.save()
+
         return commentCreated
     },
     getAll: async () => {
